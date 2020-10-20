75a68f47d53acf500425157b29179e25104091a0
Giljape <dyghks123@gmail.com>
2020-10-20 20:32:28 +0900

    JS : Home Transparent

--- main.js
+++ main.js
@@ -21,17 +21,34 @@ navbarmenu.addEventListener('click', (event) => {
         return;
     }
     scrollIntoView(link);
-
 });
 
-// Handle click on "contact me" button on home
+ // Handle click on "contact me" button on home
 const contactbtn = document.querySelector('.home__contact');
  contactbtn.addEventListener('click', (event)=> {
      scrollIntoView('#contact');
+
  });
+
+ // 스크롤 내릴때 home컨텐츠내용 점점 흐려지게 하는것
+ const homecontainer = document.querySelector('.home__container');
+ const homeheight = homecontainer.getBoundingClientRect().height;
+ const home = document.querySelector("#home");
+ document.addEventListener('scroll', () => {
+    homecontainer.style.opacity = 1-window.scrollY / homeheight;
+    contactbtn.style.opacity = 1-window.scrollY  / homeheight; 
+});
+
+contactbtn.addEventListener("mouseenter", ()=>{
+    contactbtn.style.opacity = 0.9;
+});
+
+contactbtn.addEventListener("monuseleave",()=>{
+    contactbtn.style.opacity = 1-window.scrollY  / homeheight; 
+});
 
  function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior : "smooth" });
  }
- 
+
