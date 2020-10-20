a2ee58b299060632ed8ed7c8bea04898ca6a57c3
Giljape <dyghks123@gmail.com>
2020-10-20 18:51:20 +0900

    JS : Navbar menu

--- main.js
+++ main.js
@@ -12,3 +12,22 @@ document.addEventListener('scroll', () => {
 
 });
 
+// make contact button when it is on the contact
+// Handle scrolling when tapping on the navbar menu
+const navbarmenu = document.querySelector('.navbar__menu');
+navbarmenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link ==null){
+        return;
+    }
+
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+
+    // const contactme = document.querySelector('.home__contact');
+    // contactme.addEventListener('click', ()=> {
+    //     contactme.scrollIntoView({behavior: "smooth"});
+    // });
+});
